38f7ed1aebd78c2af233fce18ccd03ba75c4e360
Cole Johnson <coleharrisjohnson@gmail.com>
2019-02-05 10:58:47 -0800

    basic page

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,36 @@ import "./App.css";
 
 import React, { Component } from "react";
 
+import storyEntries from "./storyEntries.json";
+
 class App extends Component {
+    state = {
+        storyEntry: storyEntries["init"]
+    };
+
+    handleClick = (e) => {
+        // e.target is the clicked element
+        let goto = e.target.getAttribute("goto");
+        this.setState({
+            storyEntry: storyEntries[goto]
+        });
+    }
+
+    getButtons = () => {
+        return this.state.storyEntry.choices.map((choice, i) =>
+            <button key={i} onClick={this.handleClick} goto={choice.goto}>{choice.text}</button>
+        );
+    }
+
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <h2>Welcome to React</h2>
+            <div>
+                <div>
+                    {this.state.storyEntry.text}
                 </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <div>
+                    {this.getButtons()}
+                </div>
             </div>
         );
     }
