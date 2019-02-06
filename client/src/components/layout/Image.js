e179f4c764b832bfa364394d0eb44da1711a8842
Michael Nguyen <thinknownotforever@gmail.com>
2019-02-06 13:25:37 -0800

    Button styling working, not linking though

--- client/src/components/layout/Image.js
+++ client/src/components/layout/Image.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Image extends Component {
     render() {
         return (
-            <img src={this.props.href} alt="" />
+            <img src={this.props.src} alt="" />
         );
     }
 }
