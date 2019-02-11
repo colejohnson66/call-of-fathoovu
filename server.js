0e4b82a8777db62b7e1cf1080d55fd3e013fda3a
Cole Johnson <coleharrisjohnson@gmail.com>
2019-02-11 11:15:15 -0800

    cleaned up server.js

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
+const PORT = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/callOfFathoovu";
+
+
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
 const app = express();
-var logger = require('morgan');
-var mongoose = require('mongoose');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-// Define middleware here
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
+app.use(morgan("dev"));
+if (process.env.NODE_ENV === "production")
     app.use(express.static("client/build"));
-}
-app.use(logger('dev'));
-// Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/storiesDB", { useNewUrlParser: true });
-// Define API routes here
 
-// Send every other request to the React app
-// Define any API routes before this runs
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+
+app.get("/api/story/:path", (req, res) => {
+
+});
+
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
