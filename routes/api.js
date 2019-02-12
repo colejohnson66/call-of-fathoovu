56b1012fed237d309baa2734ef553f83d317b1ce
Cole Johnson <coleharrisjohnson@gmail.com>
2019-02-12 10:44:53 -0800

    added POST /api/user route

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const StoryPath = require("../models/StoryPath");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
+
 
 router.get("/stories/:path", (req, res) => {
     StoryPath.find({ path: req.params.path }).then((data) => {
@@ -25,8 +28,27 @@ router.post("/stories/bulkInsert", (req, res) => {
 
             obj.validateSync();
             obj.save();
+
+            res.json({});
         }
-        res.json({});
+    });
+});
+
+
+router.post("/user", (req, res) => {
+    User.findOne({ username: req.body.username }).then((data) => {
+        // don't create the user if they already exist
+        if (data !== null)
+            return res.status(400).json({});
+
+        let user = new User();
+        user.username = req.body.username;
+        user.password = bcrypt.hashSync(req.body.password, 10);
+
+        user.validateSync();
+        user.save();
+
+        res.json(user);
     });
 });
 
